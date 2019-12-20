f51fabf3ffe8e1b840cb3449c41c5cb861020fb9
Paul Huang <huangbaorong@hzgit.com>
2019-12-20 10:15:43 +0800

    Initial commit

---
+++ src/main/java/org/example/controller/WelcomeController.java
@@ -0,0 +1,45 @@
+package org.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class WelcomeController {
+
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("张三", "李四", "王五");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "welcome"; //view
+    }
+
+    // /hello?name=kotlin
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
+    }
+
+    @GetMapping("/element")
+    public String mainWithParam( Model model) {
+
+        return "elementui"; //vie
+    }
+
+}
